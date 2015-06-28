aa53ad85e75041001beaeb8cd82eeb7d909f4c5e
unknown <daiying.zhang@QF-zhangdaiying.qunarservers.com>
2015-06-28 12:08:53 +0800

    dasdsd

--- src/org/simi/api/service/dao/impl/UserDao.java
+++ src/org/simi/api/service/dao/impl/UserDao.java
@@ -161,6 +161,8 @@ public class UserDao {
 						userInfoMap.put("price",rs.getString("price"));
 						userInfoMap.put("sex",rs.getString("sex"));
 						userInfoMap.put("anouncement",rs.getString("anouncement"));
+						userInfoMap.put("telephone",rs.getString("telephone"));
+						userInfoMap.put("email",rs.getString("email"));
 						return userInfoMap;
 					}
 		   });
@@ -379,6 +381,8 @@ public class UserDao {
 		sqlBuffer.append(", IFNULL(u.price,'') price");
 		sqlBuffer.append(", IFNULL(u.sex,'') sex");
 		sqlBuffer.append(", IFNULL(u.anouncement,'') anouncement");
+		sqlBuffer.append(", IFNULL(u.telephone,'') telephone");
+		sqlBuffer.append(", IFNULL(u.email,'') email");
 		sqlBuffer.append(" FROM t_file f INNER JOIN t_user u on f.pk_user = u.id AND f.fileType = '1'");
 		
 		if (!"0".equals(type)) {
@@ -427,6 +431,8 @@ public class UserDao {
 		sqlBuffer.append(", IFNULL(u.sex,'') sex");
 		sqlBuffer.append(", IFNULL(u.anouncement,'') anouncement");
 		sqlBuffer.append(", IFNULL(u.price-u.price_yestoday,0) incrument");
+		sqlBuffer.append(", IFNULL(u.telephone,'') telephone");
+		sqlBuffer.append(", IFNULL(u.email,'') email");
 		sqlBuffer.append(" FROM t_file f INNER JOIN t_user u on f.pk_user = u.id AND f.fileType = '1'");
 	
 		//0代表返回所有类型的数据
@@ -469,6 +475,8 @@ public class UserDao {
 		sqlBuffer.append(", IFNULL(u.sex,'') sex");
 		sqlBuffer.append(", IFNULL(u.price,'') price");
 		sqlBuffer.append(", IFNULL(u.anouncement,'') anouncement");
+		sqlBuffer.append(", IFNULL(u.telephone,'') telephone");
+		sqlBuffer.append(", IFNULL(u.email,'') email");
 		sqlBuffer.append(" FROM t_file f INNER JOIN t_user u on f.pk_user = u.id AND f.fileType = '1'");
 		//按照类型返回数据
 		sqlBuffer.append("  WHERE name like  '%"+searchKey+ "%'");
