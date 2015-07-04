988cca12964d3132af3212e80c34dc49cbe2699a
daiying.zhang <zdying@live.com>
2015-07-04 14:11:06 +0800

    delete fields

--- WebRoot/scripts/quiz.js
+++ WebRoot/scripts/quiz.js
@@ -190,7 +190,7 @@ $(function(){
     function addPrice(){
         var count = localStorage.getItem('quizScoreCount') || '';
         count = Number(count);
-
+alert(count);
         if(!localStorage.getItem('userid') || count > 1){
             return
         }
